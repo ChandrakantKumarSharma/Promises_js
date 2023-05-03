8bdba91aa8e1ea794b7a24ce0229d8d844620651
chandrakant <chandrkantkumarsharma@gmail.com>
2023-05-03 20:12:05 +0530

    Program

---
+++ index.js
@@ -0,0 +1,25 @@
+// function counter(){
+//     var counter = 0;
+
+//     function IncreaseCounter() {
+//         return counter += 1;
+//     };
+
+//     return IncreaseCounter;
+// }
+
+// var counter = counter();
+// alert(counter());
+// alert(counter());
+// alert(counter());
+// alert(counter());
+
+
+let count = 0;
+(function () {
+  if (count === 0) {
+    let count = 1;
+    console.log(count); // What is logged?
+  }
+  console.log(count); // What is logged?
+})();
